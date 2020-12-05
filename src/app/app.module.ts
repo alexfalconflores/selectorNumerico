36f920e7eae5396873aec19d6af99b54c3699989
Alex Falcon <alexfalconflores@hotmail.com>
2020-12-05 18:38:09 -0500

    ng generate component Selector Numerico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectorNumericoComponent } from './selector-numerico/selector-numerico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectorNumericoComponent
   ],
   imports: [
     BrowserModule,
