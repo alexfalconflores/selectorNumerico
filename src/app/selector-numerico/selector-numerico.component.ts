71ea93b908313ef4fc6136d025d62920866a9308
Alex Falcon <alexfalconflores@hotmail.com>
2020-12-05 19:01:22 -0500

    Selector Numerico terminado

--- src/app/selector-numerico/selector-numerico.component.ts
+++ src/app/selector-numerico/selector-numerico.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-selector-numerico',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorNumericoComponent implements OnInit {
 
+  @Input() minimo = '';
+  @Input() maximo = '';
+
+  actual = 0;
+
   constructor() { }
 
-  ngOnInit(): void {
+  // tslint:disable-next-line: typedef
+  ngOnInit(){
+    this.actual = Number(this.minimo);
+  }
+
+  // tslint:disable-next-line: typedef
+  incrementar(){
+    if (this.actual < Number(this.maximo)) {
+      this.actual++;
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  decrementar(){
+    if (this.actual > Number(this.minimo)) {
+      this.actual--;
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  fijar(v: number){
+    if (v >= Number(this.minimo) && v <= Number(this.maximo)){
+      this.actual = v;
+    }
   }
 
 }
